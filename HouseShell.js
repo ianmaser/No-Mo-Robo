e8c13573cce5acc47dae3a106938fac3620aa5da
JamieABall <jamiegamer93@att.net>
2015-01-29 15:43:52 -0800

    Update HouseShell.js

--- HouseShell.js
+++ HouseShell.js
@@ -2,6 +2,7 @@
    @author Jamison Ball
    @date 1/29/15
    @last update: 1/29/15
+   @last editor: //note to team members: put your name here if you edit the document
    
    *********************************************************************************************************
    The basic house class for team review
@@ -12,7 +13,7 @@ var House = function() {
 	var health = 100; 
 	var isHit = false; //just putting this here in case we need it for collision detection 
 
- //possible functions/ variables
+ //possible functions/variables
  /*
    Note: in this section is my guess as to what functions or variables this class may need functions in
    caps are things I think we will need but haven't discussed in detail
