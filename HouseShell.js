963143a42b04beb03f8428130e6761d3916d6c21
JamieABall <jamiegamer93@att.net>
2015-01-29 15:39:01 -0800

    Create HouseShell.js

---
+++ HouseShell.js
@@ -0,0 +1,24 @@
+/*
+   @author Jamison Ball
+   @date 1/29/15
+   
+   *********************************************************************************************************
+   The basic house class for team review
+   *********************************************************************************************************
+*/
+
+var House = function() {
+	var health = 100; 
+	var isHit = false; //just putting this here in case we need it for collision detection 
+
+ //possible functions/ variables
+ /*
+   Note: in this section is my guess as to what functions or variables this class may need functions in
+   caps are things I think we will need but haven't discussed in detail
+   
+   var noHealth-true if house has no health in which case we end the game
+   
+   function COLLISIONCHECK- check to see if the house has been hit
+   function UPDATE-standard update method so we can provide feedback to the player
+ */
+ }
