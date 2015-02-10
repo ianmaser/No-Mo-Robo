1ebd0d4e7cef57bc59f6fc3dac4d71459d4d2029
JamieABall <jamiegamer93@att.net>
2015-02-10 14:35:40 -0800

    Create Enemy Shell.js

---
+++ Enemy Shell.js
@@ -0,0 +1,28 @@
+/*@author: Jamison Ball
+  @date: 2/10/15
+  @last update: 2/10/15
+  @last editor:
+  
+  ****************************************************************************************************************
+  The basic Enemy class for team review
+  ******************************************************************************************************************
+  */
+var health = 100;
+var sprite;
+var damage = 5;
+var xPos;
+var yPos
+var SIZE = 20;
+var velocity = 2;
+var direction;
+/* var LEFT;
+   var UP;
+   var DOWN;
+******************************************************************************************************************
+funtions
+******************************************************************************************************************
+*/
+function move();
+function attack();
+function checkCollision();
+//function calculateShortestPath();
