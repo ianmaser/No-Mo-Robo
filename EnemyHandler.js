5a823e4ae10473400fa5b275287ed7237184f67a
JamieABall <jamiegamer93@att.net>
2015-02-12 14:52:10 -0800

    Create EnemyHandler.js

---
+++ EnemyHandler.js
@@ -0,0 +1,46 @@
+/*EnemyHandler.js
+ * **********************************************************************************************************************************************************************************************
+ * A factory class to handle the spawning of Enemies
+ * **********************************************************************************************************************************************************************************************
+ * @author Jamison Ball
+ * @date 2/12/15
+ * @last modified 2/12/15
+ * @last editor 
+ */
+
+
+function EnemyHandler()
+{
+  var length = 0;
+  var enemyList = new DList();
+  
+ this.spawn = function()
+  {
+      var e = new Enemy(this.newID());
+      enemyList.append(e);
+      length++;
+  }
+  this.removeEnemey = function(id)
+  {
+      enemyList.moveTo(id);
+      enemyList.remove();
+  }
+  this.update = function()
+  {
+      for(enemyList.moveTo(0); enemyList.getIndex() >= 0; enemyList.moveNext())
+      {
+          enemyList.getElement().update();
+      }
+  }
+  
+  this.newID = function()
+  {
+      var next;
+      
+      if(this.next === undefined || this.next < 0)
+      {
+          this.next = 0;
+      }
+      return this.next++;
+  }
+  }
