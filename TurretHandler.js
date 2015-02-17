7cad1d583174a7858a64cffcd8e3ff413c2b4a85
JamieABall <jamiegamer93@att.net>
2015-02-17 15:21:39 -0800

    Update TurretHandler.js

--- TurretHandler.js
+++ TurretHandler.js
@@ -1,7 +1,7 @@
 /* 
  * @author Jamison Ball
  * @date 2/13/15
- * @last update 2/13/15
+ * @last update 2/17/15
  * @last editor
  * *************************************************************************************************************************************************************************************************
  * A class to handle the spawning of turrets. Note to other team members: I thought it easier to include a spawn function rather than write another spawn handler specific to turrets.
@@ -11,23 +11,23 @@
 function TurretHandler(world)
 {
     var turretList = new DList();
-    var length = 0;
+    //var length = 0;
     var _MAX_TURRETS; //maximum turrets allowed on the field
 
 /*
  * @param x and @param y may be unnecesarry depending on the implementation of Turret.js.
- * Furthur this may be reworked into private function.
+ * Furthur this may be reworked into private function. May need x and y params
  */
-this.spawnTurret = function(x, y, type)
+this.spawnTurret = function(type)
 {
-   if(length < _MAX_TURRETS)
+   if(turretList.size() < _MAX_TURRETS)
    {
     var turret = new Turret(type, ID());
-    turret.x = x;
-    turret.y = y;
+  //  turret.x = x;
+  //  turret.y = y;
     turretList.append(turret);
     world.addChild(turret);
-    length++;
+   // length++
    }
    
    else
@@ -40,7 +40,14 @@ this.update = function()
 {
     for(turretList.moveTo(0); turretList.getIndex() >= 0; turretList.moveNext())
     {
-          turretList.getElement().update();
+         turretList.getElement().update();
+          
+      if(turretList.getElement().isDead())
+      {
+          turretList.getElement().removeSelf();
+          turretList.remove();
+         // length--;
+      }
     }
 };
 
@@ -53,27 +60,6 @@ function ID()
           this.next = 0;
       }
       return this.next++;
-};
-
-//method may be changed based on implementation of Turret.
-this.removeTurret = function()
-{
-  for(turretList.moveTo(0); turretList.getIndex() >= 0; turretList.moveNext())
-  {
-      var current = turretList.getElement();
-      
-      if(current.destroyed() || current.sold())
-      {
-          turretList.remove();
-          length--;
-      }
-      world.removeChild(current);
-  }
-};
-
-this.getLength = function()
-{
-    return length;
 };
 
 this.getMaxTurrets = function()
