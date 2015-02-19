a9f1d81b6eecbcbe76c169311f3bebd3b5880867
JamieABall <jamiegamer93@att.net>
2015-02-19 13:31:36 -0800

    Update global.js

--- global.js
+++ global.js
@@ -6,8 +6,8 @@ A list of global variables (to be decalred) and functions for use in different c
 @date: 2/18/15
 @last: update
 @last: editor
-
-var _MAX_TURRETS;
-var _NUM_LANES;
-var _LANE_HEIGHT;
-var _BASE_HEIGHT;
+*/
+var _MAX_TURRETS; //max number of turrets on the field
+var _NUM_LANES; //the number of lanes
+var _LANE_HEIGHT; //the height of each lane
+var _BASE_HEIGHT; // dstance between the tile grid and the bottom of the screen
