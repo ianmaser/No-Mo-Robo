f00c4a3d28e3f2d078a7dc4a65587904a95f14c1
JamieABall <jamiegamer93@att.net>
2015-02-19 13:25:53 -0800

    Create global.js

---
+++ global.js
@@ -0,0 +1,13 @@
+/*global.js
+********************************************************************************************************************************
+A list of global variables (to be decalred) and functions for use in different classes
+********************************************************************************************************************************
+@authors: Jamsion Ball
+@date: 2/18/15
+@last: update
+@last: editor
+
+var _MAX_TURRETS;
+var _NUM_LANES;
+var _LANE_HEIGHT;
+var _BASE_HEIGHT;
