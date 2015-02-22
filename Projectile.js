0e03e70b8818effd1a82b56628776f2a2bb4e287
JamieABall <jamiegamer93@att.net>
2015-02-22 11:03:14 -0800

    Create Projectile.js

---
+++ Projectile.js
@@ -0,0 +1,46 @@
+/* 
+ * @author Jamison Ball
+ * @date 2/21/15
+ * @last update 2/21/15
+ * @last editor
+ * ************************************************************************************************************************************************************************************************
+ * The basic projectile class. It might possibly go unused but it's a decent test class for collison detection
+ * ************************************************************************************************************************************************************************************************
+ */
+function Projectile(x, y)// Note to self and other team members look up function overloading in javascript. May come in handy if different projectiles have different speeds
+{
+    var sprite = new Sprite();
+    var speed = 10;
+    var damage = 1;
+    sprite.x = x;
+    sprite.y = y;
+    
+    this.draw = function()
+    {
+        world.addChild(getSprite());
+    };
+    
+    this.getSprite = function()
+    {
+        return this.sprite;
+    };
+    
+    
+    this.damage = function(damage, enemy) 
+    {
+        //to be implemented
+    };
+    
+    //@override note to team members: take your pick. Which damage function is better?
+    this.damage = function(damage)
+    {
+        //to be impemented
+    };
+    
+    this.removeSelf = function()
+    {
+        world.removeChild(this);
+    };
+};
+
+
