a7d9577db8ac54e4ca9091ebc808d14cea9101a8
JamieABall <jamiegamer93@att.net>
2015-02-27 15:43:19 -0800

    Update global.js

--- global.js
+++ global.js
@@ -11,3 +11,17 @@ var MAX_TURRETS; //max number of turrets on the field
 var NUM_LANES; //the number of lanes
 var LANE_HEIGHT; //the height of each lane
 var BASE_HEIGHT; // dstance between the tile grid and the bottom of the screen
+
+//Turret constants
+var TURRET_CONDITION;
+var TURRET_FIRE_RATE;
+var TURRET_DAMAGE;
+var TURRET_SPRITE_SRC;
+var TURRET_SPRITE_WIDTH;
+var TURRET_SPRITE_HEIGHT;
+//***********************
+
+//Projectile Constants
+var PROJECTILE_SPRITE_SRC;
+var PROJ_SPRITE_WIDTH;
+var PROJ_SPRITE_HEIGHT;
